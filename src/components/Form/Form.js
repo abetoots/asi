51ba89b274bdac18a2a81beef4734a2a358aabdd
Abe Caymo <caymo.abesuni@gmail.com>
2019-11-26 19:55:59 +0800

    handle ui on success/failure
    -add proptype validation

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,19 +1,39 @@
 import React from 'react';
-import PropTypes from 'prop-types'
-
 import './Form.scss';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import PropTypes from 'prop-types';
+
+
 const Form = (props) => {
+    let statusUi = '';
+    if (props.submitted) {
+        statusUi = <div className="Form__status -success">Success ✅ </div>
+    } else if (props.submitError) {
+        statusUi = <div className="Form__status -error">{props.submitError} ❌</div>
+    }
     return (
         <form className="Form" onSubmit={props.handleSubmit}>
-            {props.children}
-            <button className="Form__submitBtn" type="submit">{props.btnText}</button>
+            {
+                // eslint-disable-next-line react/prop-types
+                props.children
+            }
+            <button className="Form__submitBtn" disabled={props.submitting} type="submit">
+                {props.submitting ?
+                    <FontAwesomeIcon icon="spinner" spin />
+                    : props.btnText}
+            </button>
+            {statusUi}
         </form>
     );
 };
 
 Form.propTypes = {
-    handleSubmit: PropTypes.func.isRequired
+    handleSubmit: PropTypes.func.isRequired,
+    submitError: PropTypes.string,
+    submitted: PropTypes.bool,
+    submitting: PropTypes.bool,
+    btnText: PropTypes.string.isRequired
 }
 
 export default Form;
