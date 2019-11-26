aea8c0e0550959531d7ef1c1db9a38a233d68b83
Abe Caymo <caymo.abesuni@gmail.com>
2019-11-26 23:04:23 +0800

    handle toggled prop

--- src/components/UI/BurgerMenu/BurgerMenu.js
+++ src/components/UI/BurgerMenu/BurgerMenu.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import './BurgerMenu.scss';
 
-const BurgerMenu = (props) => (
-    <button className="BurgerMenu" onClick={props.clicked}>
-        <div className="BurgerMenu__bar -one"></div>
-        <div className="BurgerMenu__bar -two"></div>
-        <div className="BurgerMenu__bar -three"></div>
-        <span className="BurgerMenu__label">Menu</span>
-    </button>
-);
+import PropTypes from 'prop-types';
+
+
+const BurgerMenu = (props) => {
+    const burgerClasses = ['BurgerMenu'];
+
+    if (props.toggled) {
+        burgerClasses.push('-toggled');
+    }
+
+    return (
+        <button className={burgerClasses.join(' ')} onClick={props.handleClick}>
+            <div className="BurgerMenu__bar -one"></div>
+            <div className="BurgerMenu__bar -two"></div>
+            <div className="BurgerMenu__bar -three"></div>
+            <span className="BurgerMenu__label">Menu</span>
+        </button>
+    );
+}
+
+BurgerMenu.propTypes = {
+    handleClick: PropTypes.func.isRequired,
+    togggled: PropTypes.bool
+}
+
 
 export default BurgerMenu;
