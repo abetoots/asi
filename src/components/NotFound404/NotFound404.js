f2f8c5d073327cbeb3d9a8e1736ba35a70c27476
Abe Caymo <caymo.abesuni@gmail.com>
2019-11-26 23:03:40 +0800

    fix classname

--- src/components/NotFound404/NotFound404.js
+++ src/components/NotFound404/NotFound404.js
@@ -3,9 +3,9 @@ import './NoutFound404.scss';
 import { NavLink } from 'react-router-dom';
 
 const NotFound404 = (props) => (
-    <div class="NotFound404">
-        <h1 class="NotFound404__title">404 Page Not Found</h1>
-        <p class="NotFound404__subText">We can't find what you're looking for. <NavLink to="/">Back to Home</NavLink> </p>
+    <div className="NotFound404">
+        <h1 className="NotFound404__title">404 Page Not Found</h1>
+        <p className="NotFound404__subText">We can't find what you're looking for. <NavLink to="/">Back to Home</NavLink> </p>
     </div>
 );
 
