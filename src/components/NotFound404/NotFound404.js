0b8bcf4f396ca975206dbfb26569fc2003cc79fa
Abe Caymo <caymo.abesuni@gmail.com>
2019-11-26 19:59:32 +0800

    add notfound styles, add redirect link

--- src/components/NotFound404/NotFound404.js
+++ src/components/NotFound404/NotFound404.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import './NoutFound404.scss';
+import { NavLink } from 'react-router-dom';
 
 const NotFound404 = (props) => (
-    <div className="NotFound404">
-        <h1>Not Found</h1>
+    <div class="NotFound404">
+        <h1 class="NotFound404__title">404 Page Not Found</h1>
+        <p class="NotFound404__subText">We can't find what you're looking for. <NavLink to="/">Back to Home</NavLink> </p>
     </div>
 );
 
