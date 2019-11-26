1365afaf48e967dc5fa57ef5b4a5c67b2c3633d2
Abe Caymo <caymo.abesuni@gmail.com>
2019-11-26 23:04:03 +0800

    add async lazyloading logic for notfound

--- src/components/RoutesList/RoutesList.js
+++ src/components/RoutesList/RoutesList.js
@@ -3,21 +3,26 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 //Components
 import Aux from '../../hoc/Auxiliary';
-import NotFound from '../NotFound404/NotFound404';
+import asyncComponent from '../../hoc/asyncComponent/asyncComponent';
 
 import PropTypes from 'prop-types';
 
+const AsyncNotFound = asyncComponent(() => {
+    return import('../NotFound404/NotFound404');
+})
+
 const RoutesList = (props) => {
+
 
     return (
         <Aux>
             {/* For a given set of routes, load only one of each */}
             <Switch>
                 {props.routes.map(route =>
-                    <Route key={route.path} exact={route.exact} path={route.path} component={route.component} />
+                    <Route key={route.path} exact={route.exact} path={route.path} component={route.component ? route.component : AsyncNotFound} />
                 )}
                 {/* Handle our 404 case. Must be defined last */}
-                <Route component={NotFound} />
+                <Route component={AsyncNotFound} />
             </Switch>
         </Aux>
     );
