ee4824320f0078234691646a43dfe9f8533ea509
Abe Caymo <caymo.abesuni@gmail.com>
2019-11-28 07:31:31 +0800

    replace logo, adjust margin

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './Logo.scss';
 
-import logo from '../../assets/logo.svg';
+import logo from '../../assets/logo 2.svg';
 
 const Logo = (props) => (
     <div className="Logo">
         <img className="Logo__img" src={logo} alt="ASI Logo" />
-        <div className="Logo__text">ASI</div>
     </div>
 );
 
