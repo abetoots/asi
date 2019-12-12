bff7f34f10809807e984353f996bf64a8281fc46
Abe Caymo <caymo.abesuni@gmail.com>
2019-12-12 14:28:59 +0800

    replace logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Logo.scss';
 
-import logo from '../../assets/images/logo 2.svg';
+import logo from '../../assets/images/asi-icon.svg';
 
 const Logo = (props) => (
     <div className="Logo">
