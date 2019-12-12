ab5eda96579ad12bdee55c14899bcf56a1b9e3ec
Abe Caymo <caymo.abesuni@gmail.com>
2019-12-12 11:23:14 +0800

    add copywebpackplugin, add source map, remove favicon

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,26 +5,33 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 
 module.exports = merge(common, {
     mode: 'production',
+    devtool: 'nosources-source-map',
     output: {
         filename: '[name].[contentHash].bundle.js',
         path: path.resolve(__dirname, 'build'),
     },
     plugins: [
-        new MiniCssExtractPlugin({ filename: '[name].[contentHash].css' }),
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
-            template: "./public/template.html",
+            template: "public/index.html",
             minify: {
                 removeComments: true,
                 collapseWhitespace: true,
                 removeAttributeQuotes: true
             },
-            favicon: './public/asi-logo@0.5x.png'
-        })
+        }),
+        new CopyPlugin([
+            { from: 'public/img', to: 'img/' },
+            { from: 'public/css/shell.min.css', to: 'css/' },
+            { from: 'public/css/shell.min.css.map', to: 'css/' },
+            'public/manifest.json'
+        ]),
+        new MiniCssExtractPlugin({ filename: '[name].[contentHash].css' }),
     ],
     module: {
         rules: [
