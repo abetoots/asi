4d3a0fe413dc3309d9b280a99e69e380bc1099e7
Abe Caymo <caymo.abesuni@gmail.com>
2019-12-27 14:04:44 +0800

    add facebook form

--- src/misc/shared/forms.js
+++ src/misc/shared/forms.js
@@ -109,11 +109,25 @@ export const accountForm = [
         elementType: 'input',
         elementConfig: {
             type: 'url',
+            //TODO update firestore validation
             pattern: /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/
         },
         label: 'Website',
         customProps: {
             icon: ['fas', 'globe'],
+            insideInput: true
+        },
+    },
+    {
+        key: 'facebook',
+        elementType: 'input',
+        elementConfig: {
+            type: 'url',
+            pattern: /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/
+        },
+        label: 'Facebook',
+        customProps: {
+            icon: ['fab', 'facebook'],
             insideInput: true
         },
     },
