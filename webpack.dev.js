cb9a13f6c99168cd7c6a3ffeefd65d1de286e1a5
Abe Suni M. Caymo <45987443+abetoots@users.noreply.github.com>
2020-03-24 21:00:56 +0800

    update resources, update formatting

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,56 +1,60 @@
-const path = require('path');
-const common = require('./webpack.common');
-const merge = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const path = require("path");
+const webpack = require("webpack");
+const common = require("./webpack.common");
+const merge = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = merge(common, {
-    mode: 'development',
-    devtool: 'eval-source-map',
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'build'),
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./public/index.html",
-        })
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    // Creates `style` nodes from JS strings
-                    'style-loader',
-                    // Translates CSS into CommonJS
-                    'css-loader',
-                    // Compiles Sass to CSS
-                    'sass-loader',
-                    {
-                        loader: 'sass-resources-loader',
-                        options: {
-                            resources: [
-                                './styles/util/_variables.scss',
-                                './styles/tools/*.scss',
-                            ]
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.css$/i,
-                use: [
-                    // Creates `style` nodes from JS strings
-                    'style-loader',
-                    // Translates CSS into CommonJS
-                    'css-loader'
-                ]
-            },
+  mode: "development",
+  devtool: "eval-source-map",
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "build")
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html"
+    }),
+    new webpack.DefinePlugin({
+      BASE_URL: JSON.stringify("http://localhost/alsi/"),
+      BASE_API_URL: JSON.stringify("http://localhost/alsi/wp-json/wp/v2/")
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+          {
+            loader: "sass-resources-loader",
+            options: {
+              resources: [
+                "./src/styles/util/_variables.scss",
+                "./src/styles/tools/*.scss"
+              ]
+            }
+          }
         ]
-    },
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true //server index.html for any route not found
-    }
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader"
+        ]
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+    historyApiFallback: true //server index.html for any route not found
+  }
 });
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,105 +1,108 @@
-const path = require('path');
-const webpack = require('webpack');
-const common = require('./webpack.common');
-const merge = require('webpack-merge');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const common = require("./webpack.common");
+const merge = require("webpack-merge");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const { InjectManifest } = require("workbox-webpack-plugin");
 
 module.exports = merge(common, {
-    mode: 'production',
-    devtool: 'nosources-source-map',
-    output: {
-        filename: '[name].[contentHash].bundle.js',
-        path: path.resolve(__dirname, 'build'),
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            template: "public/index.html",
-            minify: {
-                removeComments: true,
-                collapseWhitespace: true,
-                removeAttributeQuotes: true
-            },
-        }),
-        new CopyPlugin([
-            { from: 'public/img', to: 'img/' },
-            'public/robots.txt',
-            'public/manifest.json'
-        ]),
-        new MiniCssExtractPlugin({ filename: '[name].[contentHash].css' }),
-        new webpack.HashedModuleIdsPlugin(), // so that file hashes don't change unexpectedly
-        new InjectManifest({ // ! must be last step
-            swSrc: './src/service-worker.js'
-        })
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    // Moves CSS into separate files
-                    MiniCssExtractPlugin.loader,
-                    // Translates CSS into CommonJS
-                    'css-loader',
-                    // Compiles Sass to CSS
-                    'sass-loader',
-                    {
-                        loader: 'sass-resources-loader',
-                        options: {
-                            resources: [
-                                './styles/util/_variables.scss',
-                                './styles/tools/*.scss',
-                            ]
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.css$/i,
-                use: [
-                    // Creates `style` nodes from JS strings
-                    'style-loader',
-                    // Translates CSS into CommonJS
-                    'css-loader'
-                ]
-            },
+  mode: "production",
+  devtool: "nosources-source-map",
+  output: {
+    filename: "[name].[contentHash].bundle.js",
+    path: path.resolve(__dirname, "build")
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "public/index.html",
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeAttributeQuotes: true
+      }
+    }),
+    new CopyPlugin([
+      { from: "public/img", to: "img/" },
+      "public/robots.txt",
+      "public/manifest.json"
+    ]),
+    new MiniCssExtractPlugin({ filename: "[name].[contentHash].css" }),
+    new webpack.HashedModuleIdsPlugin(), // so that file hashes don't change unexpectedly
+    new InjectManifest({
+      // ! must be last step
+      swSrc: "./src/service-worker.js"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Moves CSS into separate files
+          MiniCssExtractPlugin.loader,
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+          {
+            loader: "sass-resources-loader",
+            options: {
+              resources: [
+                "./src/styles/util/_variables.scss",
+                "./src/styles/tools/*.scss"
+              ]
+            }
+          }
         ]
-    },
-    optimization: {
-        minimizer: [
-            new TerserPlugin({
-                sourceMap: true, // Must be set to true if using source-maps in production
-                terserOptions: {
-                    compress: {
-                        drop_console: true,
-                    },
-                }
-            })
-        ],
-        // https://medium.com/hackernoon/the-100-correct-way-to-split-your-chunks-with-webpack-f8a9df5b7758
-        runtimeChunk: 'single',
-        splitChunks: {
-            chunks: 'all',
-            maxInitialRequests: Infinity,
-            minSize: 0,
-            cacheGroups: {
-                vendors: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name(module) {
-                        // get the name. E.g. node_modules/packageName/not/this/part.js
-                        // or node_modules/packageName
-                        const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
-
-                        // npm package names are URL-safe, but some servers don't like @ symbols
-                        return `npm.${packageName.replace('@', '')}`;
-                    },
-                },
-            },
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader"
+        ]
+      }
+    ]
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        sourceMap: true, // Must be set to true if using source-maps in production
+        terserOptions: {
+          compress: {
+            drop_console: true
+          }
         }
+      })
+    ],
+    // https://medium.com/hackernoon/the-100-correct-way-to-split-your-chunks-with-webpack-f8a9df5b7758
+    runtimeChunk: "single",
+    splitChunks: {
+      chunks: "all",
+      maxInitialRequests: Infinity,
+      minSize: 0,
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            // get the name. E.g. node_modules/packageName/not/this/part.js
+            // or node_modules/packageName
+            const packageName = module.context.match(
+              /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+            )[1];
+
+            // npm package names are URL-safe, but some servers don't like @ symbols
+            return `npm.${packageName.replace("@", "")}`;
+          }
+        }
+      }
     }
+  }
 });
